9b5aada09f437b777ee36c11e7ec9efc94d02e4f
brezzz <brezzz@users.noreply.github.com>
2019-12-18 16:37:39 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const movie = require('./movies-data-small.json')
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+
+
+app.get('/movie', function getMovie(req, res) {
+
+    
+  let response = movie;
+
+  
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      // case insensitive searching
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  
+ //... repeat for country and avg_vote
+ if (req.query.country) {
+  response = response.filter(movie =>
+    // case insensitive searching
+    movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+  )
+}
+
+
+if (req.query.avg_vote) {
+  response = response.filter(genre =>
+    // case insensitive searching
+    Number(movie.avg_vote) >= Number(req.query.avg_vote)
+  )
+}
+
+  res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
+//uuid 2fc1c50a-1926-11ea-90b3-2e728ce88125
